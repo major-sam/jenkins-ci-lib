d928e666ad96ab364939f4a234c4fafc278bcaed
vrebyachih <vrebyachih@baltbet.ru>
2021-12-22 18:48:54 +0300

    update artifact url

--- vars/addArtifactLink.groovy
+++ vars/addArtifactLink.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]){          
 	def url = "${config.nexusRoot}/${config.buildname}/${config.buildname}/${config.gitBranch}/${config.version}"
-	writeFile file: 'link.html', text:  url
+	writeFile file: 'link.html', text:	"<a href=${url}>Nexus Artifact </a> <br>" 
 	archiveArtifacts allowEmptyArchive: true, artifacts: 'link.html', caseSensitive: false, followSymlinks: false
 }
