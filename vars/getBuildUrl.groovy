08fe0fcf252973ee40a17a9dd981f21faa0996ad
vrebyachih <vrebyachih@baltbet.ru>
2021-12-22 19:25:31 +0300

    fix descr

--- vars/getBuildUrl.groovy
+++ vars/getBuildUrl.groovy
@@ -1,5 +1,6 @@
 def call(Map config = [:]){          
-	def url = "${config.nexusRoot}:${config.buildname}:${config.buildname}" +
+	def url = "${config.nexusRoot}${config.buildname}:${config.buildname}" +
 		"%2F${config.gitBranch}%2F${config.version}%2F${config.gitBranch}-${config.version}-${config.configType}.zip"
+	currentBuild.description = currentBuild.description  +"<br><a href=${url}>Nexus Artifact </a> <br>"
 	return "<br><a href=${url}>Nexus Artifact </a> <br>"
 }
