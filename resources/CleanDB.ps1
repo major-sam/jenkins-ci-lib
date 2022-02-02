1b09d9224ff9eba7615699c3c1112b9cf13f3287
vrebyachih <vrebyachih@baltbet.ru>
2022-02-02 15:14:51 +0300

    add cleanup

--- resources/CleanDB.ps1
+++ resources/CleanDB.ps1
@@ -1,4 +1,5 @@
-EXEC sp_MSforeachdb
+Import-Module -Name SqlServer -Force
+$rQuery =" EXEC sp_MSforeachdb
   'IF DB_ID(''?'') > 4
   BEGIN
     print ''remove db ?''
@@ -6,3 +7,5 @@ EXEC sp_MSforeachdb
     DROP DATABASE [?];
     '' )
   END'
+  "
+invoke-sqlcmd  -verbose -ServerInstance $env:computername -Query $rQuery
